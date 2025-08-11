dbfc88ac474aeabe0b102f760b4d7efd91c5be38
emil <karlemil.magnusson@gmail.com>
2025-08-11 21:18:37 +0200

    Initial Commit

---
+++ LiveTrackerTests/LiveTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTrackerTests.swift
+//  LiveTrackerTests
+//
+//  Created by Emil Magnusson on 2025-08-11.
+//
+
+import Testing
+@testable import LiveTracker
+
+struct LiveTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
